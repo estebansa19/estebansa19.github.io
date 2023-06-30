ef50906a4503c13a9741e0116e4ef9abf061c106
Esteban Saldarriaga Alzate <estebansa@outlook.com>
2023-06-29 23:13:48 -0500

    feature: Project working fine

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Date from '../components/date';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+
+  return {
+    props: {
+      allPostsData,
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return(
     <Layout home>
       <Head>
@@ -12,6 +25,22 @@ export default function Home() {
         <p>Hello 👋, I'm <strong>Esteban</strong>, I’m a software engineer located in Colombia.</p>
         <p>I'm 23 years old, a Ruby/Ruby on Rails lover; I like JavaScript as well but, Ruby is my main.</p>
         <p>I like reading a lot, playing some videogames and I also like watching some series and anime 📺.</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {
+            allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>{title}</Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date}/>
+                </small>
+              </li>
+            ))
+          }
+        </ul>
       </section>
     </Layout>
   );
